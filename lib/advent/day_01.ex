85dc8e1c9f02d34afc8c059512be691e2d6fee71
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2019-12-01 15:33:54 +0100

    Day 1 part 2

--- lib/advent/day_01.ex
+++ lib/advent/day_01.ex
@@ -22,17 +22,62 @@ defmodule Advent.Day01 do
   The Fuel Counter-Upper needs to know the total fuel requirement. To find it, individually calculate the fuel needed for the mass of each module (your puzzle input), then add together all the fuel values.
 
   What is the sum of the fuel requirements for all of the modules on your spacecraft?
+
+  --- Part Two ---
+
+  During the second Go / No Go poll, the Elf in charge of the Rocket Equation Double-Checker stops the launch sequence. Apparently, you forgot to include additional fuel for the fuel you just added.
+
+  Fuel itself requires fuel just like a module - take its mass, divide by three, round down, and subtract 2. However, that fuel also requires fuel, and that fuel requires fuel, and so on. Any mass that would require negative fuel should instead be treated as if it requires zero fuel; the remaining mass, if any, is instead handled by wishing really hard, which has no mass and is outside the scope of this calculation.
+
+  So, for each module mass, calculate its fuel and add it to the total. Then, treat the fuel amount you just calculated as the input mass and repeat the process, continuing until a fuel requirement is zero or negative. For example:
+
+      A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2 divided by 3 and rounded down is 0, which would call for a negative fuel), so the total fuel required is still just 2.
+      At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel, which requires 21 fuel, which requires 5 fuel, which requires no further fuel. So, the total fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
+      The fuel required by a module of mass 100756 and its fuel is: 33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
+
+  What is the sum of the fuel requirements for all of the modules on your spacecraft when also taking into account the mass of the added fuel? (Calculate the fuel requirements for each module separately, then add them all up at the end.)
   """
 
   @doc """
-  Returns the total amount of fuel needed for the module weights given in the input
+  Part 1
   """
   @spec required_fuel(String.t()) :: integer
   def required_fuel(input) do
     input
+    |> parse
+    |> Enum.map(&single_required_fuel/1)
+    |> Enum.sum()
+  end
+
+  @doc """
+  Part 2
+  """
+  @spec required_fuel_recursive(String.t()) :: integer
+  def required_fuel_recursive(input) do
+    input
+    |> parse
+    |> Enum.map(&single_required_fuel/1)
+    |> Enum.map(&add_fuel_recursively/1)
+    |> Enum.sum()
+  end
+
+  defp add_fuel_recursively(mass) when mass > 0 do
+    new_mass = mass |> single_required_fuel() |> add_fuel_recursively()
+    mass + new_mass
+  end
+
+  defp add_fuel_recursively(0), do: 0
+
+  defp single_required_fuel(mass) do
+    case div(mass, 3) - 2 do
+      n when n > 0 -> n
+      n when n <= 0 -> 0
+    end
+  end
+
+  defp parse(input) do
+    input
     |> String.split("\n", trim: true)
     |> Enum.map(&String.to_integer/1)
-    |> Enum.map(&(div(&1, 3) - 2))
-    |> Enum.sum()
   end
 end
--- test/day_01_test.exs
+++ test/day_01_test.exs
@@ -41,4 +41,55 @@ defmodule Advent.Day01Test do
       assert Day01.required_fuel(input) == 3_231_195
     end
   end
+
+  describe "required_fuel_recursive" do
+    test "example 1" do
+      input = """
+      12
+      """
+
+      assert Day01.required_fuel_recursive(input) == 2
+    end
+
+    test "example 2" do
+      input = """
+      14
+      """
+
+      assert Day01.required_fuel_recursive(input) == 2
+    end
+
+    test "example 3" do
+      input = """
+      1969
+      """
+
+      assert Day01.required_fuel_recursive(input) == 966
+    end
+
+    test "example 4" do
+      input = """
+      100756
+      """
+
+      assert Day01.required_fuel_recursive(input) == 50_346
+    end
+
+    test "multiple values" do
+      input = """
+      14
+      14
+      14
+      14
+      14
+      """
+
+      assert Day01.required_fuel_recursive(input) == 10
+    end
+
+    test "real input" do
+      input = File.read!("input_files/day_01.txt")
+      assert Day01.required_fuel_recursive(input) == 4_843_929
+    end
+  end
 end
