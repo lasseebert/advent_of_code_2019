0278c21e45316fdcacfb767185512277ea02ec33
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2019-12-01 10:58:11 +0100

    Init mix project

---
+++ mix.exs
@@ -0,0 +1,27 @@
+defmodule Advent.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :advent,
+      version: "0.1.0",
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      dialyzer: [plt_add_apps: [:ex_unit]]
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:credo, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
