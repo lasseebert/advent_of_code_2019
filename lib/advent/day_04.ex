27724db3a66e18ee2e9ba85c2ae12ddceb802199
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2019-12-05 20:52:28 +0100

    Moved puzzle texts to its own files

--- lib/advent/day_04.ex
+++ lib/advent/day_04.ex
@@ -1,40 +1,5 @@
 defmodule Advent.Day04 do
   @moduledoc """
-  --- Day 4: Secure Container ---
-
-  You arrive at the Venus fuel depot only to discover it's protected by a password. The Elves had written the password on a sticky note, but someone threw it out.
-
-  However, they do remember a few key facts about the password:
-
-      It is a six-digit number.
-      The value is within the range given in your puzzle input.
-      Two adjacent digits are the same (like 22 in 122345).
-      Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
-
-  Other than the range rule, the following are true:
-
-      111111 meets these criteria (double 11, never decreases).
-      223450 does not meet these criteria (decreasing pair of digits 50).
-      123789 does not meet these criteria (no double).
-
-  How many different passwords within the range given in your puzzle input meet these criteria?
-
-  --- Part Two ---
-
-  An Elf just remembered one more important detail: the two adjacent matching digits are not part of a larger group of matching digits.
-
-  Given this additional criterion, but still ignoring the range rule, the following are now true:
-
-      112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long.
-      123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
-      111122 meets the criteria (even though 1 is repeated more than twice, it still contains a double 22).
-
-  How many different passwords within the range given in your puzzle input meet all of the criteria?
-  """
-
-  @doc """
-  Part 1
-
   Algorithm:
 
   Start by increasing numbers to first "non-decreasing digits" number
@@ -42,9 +7,10 @@ defmodule Advent.Day04 do
   Increase by always keeping the non-decreasing constraint
   Check the other constraint for each yielded number
 
-  Runs in ~1.0 ms on my machine
-
+  Runs in ~1 ms on my machine
   """
+
+  @doc "Part 1"
   def count_valid_passwords(from, to) do
     [from, to] =
       [from, to]
@@ -55,12 +21,7 @@ defmodule Advent.Day04 do
     count_valid_passwords(from, to, 0, false)
   end
 
-  @doc """
-  Part 2
-
-  Same as part 1, but sends a flag that it should use the "extended" second constraint
-  Runs in ~1.5 ms on my machine
-  """
+  @doc "Part 2"
   def count_extended_valid_passwords(from, to) do
     [from, to] =
       [from, to]
