ee4f03baaf35bcb1d20a1ccc150b3fdd9e04e24f
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2020-01-04 00:13:33 +0100

    Day 12 part 2

--- lib/advent/day_12.ex
+++ lib/advent/day_12.ex
@@ -8,22 +8,66 @@ defmodule Advent.Day12 do
     input
     |> parse()
     |> Enum.map(&build_moon/1)
-    |> step(steps)
+    |> step_count(steps)
     |> Enum.map(&energy/1)
     |> Enum.sum()
+  end
+
+  @doc """
+  Part 2
+
+  Find repeat on individual axis and then calculate least common multiple of them, which will result in the total
+  repeat
+
+  Runs in ~1300ms.
+
+  Optimizations that is not done yet:
+   - Only run the simulation once until all axis repeats has been found. Right now it runs the simulation once per axis
+  """
+  def steps_to_repeat(input) do
+    moons =
+      input
+      |> parse()
+      |> Enum.map(&build_moon/1)
+
+    0..2
+    |> Enum.map(&find_axis_repeat(moons, &1))
+    |> Enum.reduce(&lcm/2)
+  end
+
+  defp find_axis_repeat(moons, axis) do
+    key_stream =
+      moons
+      |> Stream.unfold(fn moons ->
+        key = moons |> Enum.flat_map(fn %{pos: pos, v: v} -> [elem(pos, axis), elem(v, axis)] end)
+        {key, step(moons)}
+      end)
+
+    first = key_stream |> Enum.at(0)
+
+    key_stream
+    |> Stream.with_index()
+    |> Stream.drop(1)
+    |> Enum.find(&(elem(&1, 0) == first))
+    |> elem(1)
   end
 
   defp energy(%{pos: {x, y, z}, v: {vx, vy, vz}}) do
     (abs(x) + abs(y) + abs(z)) * (abs(vx) + abs(vy) + abs(vz))
   end
 
-  defp step(moons, 0), do: moons
+  defp step_count(moons, 0), do: moons
 
-  defp step(moons, steps) do
+  defp step_count(moons, steps) do
+    moons
+    |> step()
+    |> step_count(steps - 1)
+  end
+
+  defp step(moons) do
     moons
     |> apply_gravity()
     |> apply_velocity()
-    |> step(steps - 1)
   end
 
   defp apply_gravity(moons) do
@@ -37,8 +81,8 @@ defmodule Advent.Day12 do
 
     moons
     |> Enum.map(&{&1, moons -- [&1]})
-    |> Enum.map(fn {%{pos: {mx, my, mz}, v: {vx, vy, vz}} = moon, other_moons} ->
-      {dx, dy, dz} =
+    |> Enum.map(fn {%{pos: {mx, my, mz}, v: velocity} = moon, other_moons} ->
+      dv =
         other_moons
         |> Enum.reduce({0, 0, 0}, fn %{pos: {ox, oy, oz}}, {dx, dy, dz} ->
           {
@@ -48,17 +92,19 @@ defmodule Advent.Day12 do
           }
         end)
 
-      v = {vx + dx, vy + dy, vz + dz}
+      v = add(velocity, dv)
       Map.put(moon, :v, v)
     end)
   end
 
   defp apply_velocity(moons) do
     moons
-    |> Enum.map(fn %{pos: {x, y, z}, v: {vx, vy, vz}} = moon ->
-      Map.put(moon, :pos, {x + vx, y + vy, z + vz})
+    |> Enum.map(fn %{pos: pos, v: velocity} = moon ->
+      %{moon | pos: add(pos, velocity)}
     end)
   end
+
+  defp add({x1, y1, z1}, {x2, y2, z2}), do: {x1 + x2, y1 + y2, z1 + z2}
 
   defp build_moon(pos) do
     %{pos: pos, v: {0, 0, 0}}
@@ -75,4 +121,6 @@ defmodule Advent.Day12 do
       |> List.to_tuple()
     end)
   end
+
+  defp lcm(a, b), do: div(a * b, Integer.gcd(a, b))
 end
--- test/day_12_test.exs
+++ test/day_12_test.exs
@@ -32,4 +32,34 @@ defmodule Advent.Day12Test do
       assert Day12.total_energy(input, 1000) == 10_664
     end
   end
+
+  describe "part 2" do
+    test "example 1" do
+      input = """
+      <x=-1, y=0, z=2>
+      <x=2, y=-10, z=-7>
+      <x=4, y=-8, z=8>
+      <x=3, y=5, z=-1>
+      """
+
+      assert Day12.steps_to_repeat(input) == 2772
+    end
+
+    test "example 2" do
+      input = """
+      <x=-8, y=-10, z=0>
+      <x=5, y=5, z=10>
+      <x=2, y=-7, z=3>
+      <x=9, y=-8, z=-3>
+      """
+
+      assert Day12.steps_to_repeat(input) == 4_686_774_924
+    end
+
+    @tag :puzzle_input
+    test "puzzle_input" do
+      input = File.read!("input_files/day_12.txt")
+      assert Day12.steps_to_repeat(input) == 303_459_551_979_256
+    end
+  end
 end
